60bfce7f0dedf87ef0df5b612cf9d947fdf566d8
Simon Wood <simon.wood@holidayextras.com>
2013-06-21 14:33:44 +0100

    Added in date formatting

--- content/posts/2013-06-20-my-first-post.md
+++ content/posts/2013-06-20-my-first-post.md
@@ -1,6 +1,6 @@
 ---
 title: "My first post"
-created_at: 2012-02-10 09:00:00 +0000
+created_at: 2013-06-10 09:00:00 +0000
 kind: article
 ---
 
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,3 +5,11 @@ include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+module PostHelper
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %d, %Y')
+  end
+end
+
+include PostHelper
