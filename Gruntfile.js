3d6ad98429d2c6d6a88f45b3e22cae83a45e623f
Simon Wood <simon.wood@holidayextras.com>
2014-05-13 21:04:00 +0100

    Changed port number in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 9009,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
